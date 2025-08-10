67621b313ec2657a019dabf1e54427e646c30ccb
sachin4481 <sachin889023@gmail.com>
2025-08-11 00:38:35 +0530

    initilizing_project

---
+++ src/main/java/com/example/TaskManager_spring_boot/TaskManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.TaskManager_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TaskManager_spring_boot/TaskManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskManager_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
